7d15fbd4d8b56e862b452cadca699e96e5b0e4a5
Alan Professor <alan@iosi.com>
2021-03-23 18:57:06 -0300

    Updates in routes

--- course/Services/BaseService.swift
+++ course/Services/BaseService.swift
@@ -6,29 +6,80 @@
 //
 
 import Foundation
+
+enum IosiApi {
+    
+    case verifyEmail(String)
+    case login(String, String)
+    
+    private var baseUrl: String {
+        "https://iosi.herokuapp.com"
+    }
+    
+    private var method: String {
+        switch self {
+        case .verifyEmail:
+            return "GET"
+        case .login:
+            return "GET"
+        }
+    }
+    
+    private var body: [String: String] {
+        switch self {
+        case .verifyEmail:
+            return [:]
+        case .login(let email, let password):
+            return ["email": email, "password": password]
+        }
+    }
+    
+    private var uri: String {
+        switch self {
+        case .verifyEmail(let email):
+            return "/users/hasAccount/\(email)"
+        case .login(_, _):
+            return "/users/login"
+        }
+    }
+    
+    var request: URLRequest? {
+        
+        guard let url = URL(string: baseUrl + self.uri) else {
+            return nil
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = self.method
+        request.httpBody = try? JSONEncoder().encode(self.body)
+        return request
+    }
+}
+
 class BaseService {
     
-    private let baseUrl = "https://api.pexels.com/v1"
-    
-    func mountParameters(_ route:String, _ parameters:[ String : String ]? ) -> URLComponents {
-        var urlComponents = URLComponents(string: baseUrl + route)
-        
+    let baseURL = "https://iosi.herokuapp.com"
+
+    func mountParameters(_ route: String, _ parameters:[ String : String ]? ) -> URLComponents {
+        var urlComponents = URLComponents(string: baseURL + route)
+
         var items:[URLQueryItem] = []
-        
+
         if let params = parameters {
             for param in params {
                 items.append(URLQueryItem(name: param.key, value: param.value))
             }
         }
-        
+
         urlComponents?.queryItems = items
-        
+
         return urlComponents!
     }
     
-    func request<T:Decodable>(route:URLRequest, _ completion: @escaping (Result<T, Error>) -> Void)
-    {
-        URLSession.shared.dataTask(with: route) { (data, response, error) in
+    func request<T:Decodable>(route: IosiApi, _ completion: @escaping (Result<T, Error>) -> Void) {
+        guard let request = route.request else { return }
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let error = error {
                 completion(.failure(error))
             }
@@ -43,5 +94,23 @@ class BaseService {
             }
         }.resume()
     }
+    
+//    func request<T:Decodable>(route:URLRequest, _ completion: @escaping (Result<T, Error>) -> Void)
+//    {
+//        URLSession.shared.dataTask(with: route) { (data, response, error) in
+//            if let error = error {
+//                completion(.failure(error))
+//            }
+//
+//            let customDecoder = JSONDecoder.init();
+//
+//            if let data = data {
+//                let result = try? customDecoder.decode(T.self, from: data)
+//                completion(.success(result!))
+//            } else {
+//                print("Error Loading Data")
+//            }
+//        }.resume()
+//    }
     
 }
